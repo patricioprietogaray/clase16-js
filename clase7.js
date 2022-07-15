af84b6acdcb0ada3124b2cd5717aa39135175edc
patricioprietogaray <patricioprietogaray@gmail.com>
2022-07-15 01:26:20 -0300

    master

--- clase7.js
+++ clase7.js
@@ -125,3 +125,99 @@ console.log(user1.login().logout()); //retorna el this + login + logout
 document.write('<h2> Titulo en h2 desde JS'); //es una mala practica
 
 ////////////////
+
+
+//53:00 break
+console.log('muestra los objetos (sin ;)');
+console.log(document)
+console.log(document.doctype)
+console.log(document.documentElement)
+console.log(document.head)
+console.log(document.body)
+
+console.log('muestra el código (con ;)');
+
+console.log(document)
+console.log(document.doctype)
+console.log(document.documentElement)
+console.log(document.head)
+console.log(document.body)
+
+console.log('seleccionar un elemento por nombre: "li"');
+console.log(document.getElementsByTagName('li'));
+console.log('seleccionar un elemento por class: "item"');
+console.log(document.getElementsByClassName('item'));
+console.log('seleccionar un elemento por nombre: "name"');
+console.log(document.getElementsByName('name'));
+console.log('seleccionar un elemento por nombre: "phome"');
+console.log(document.getElementsByName('phome'));
+console.log('seleccionar un elemento por nombre: "email"');
+console.log(document.getElementsByName('email'));
+console.log('seleccionar un elemento por id (es único): "titulo"');
+console.log(document.getElementById('titulo'));
+
+console.log('Seleccionar como css con querySelector');
+console.log('Seleccionar una etiqueta (li: la primera que encuentra)');
+const lista = document.querySelector('li');
+console.log(lista);
+
+console.log('Seleccionar una clase (item: tambien el primero)');
+const item = document.querySelector('.item');
+console.log(item);
+
+console.log('Seleccionar una id (titulo: tambien el primero)');
+const titulo = document.querySelector('#titulo');
+console.log(titulo);
+
+console.log('Seleccionar una clase con todos sus componentes (item)');
+const todosLosItems = document.querySelectorAll('.item');
+console.log(todosLosItems);
+
+console.log('Seleccionar una etiqueta li con todos sus componentes');
+const todosLosLi = document.querySelectorAll('li');
+console.log(todosLosLi);
+
+console.log('Otra forma de mostrar lo anterior (li)');
+const todosLosLis = document.querySelectorAll('li');
+todosLosLis.forEach(li => console.log(li));
+/* equivale a:
+arr.forEach(function callback(currentValue, index, array) {
+    ... tu iterador
+}[, thisArg]);*/
+
+const listaEP = document.querySelector("ul").getAttribute("name");
+// todos los atributos name que se encuentren en la etiqueta ul.
+console.log(listaEP);
+
+// get lee
+// set escribe
+
+const listado = document.querySelector("ol");
+console.log(listado); // muestra todo la etiqueta ol y li
+// setTimeout(1000);
+listado.setAttribute("name", "listaSeteada");
+// muestra el valor del atributo name   ->   listaSeteada
+console.log(listado.getAttribute("name"));
+
+// de esta forma reescribo lo que tiene listado habilitar para ver los cambios
+// listado.textContent="reescribo el contenido de ol (chau li) con formato de texto";
+// listado.innerHTML="<h1>Soy una lista (código HTML innerHTML) puedo reemplazar contenido respeta etiqueta que lo contiene</h1>";
+// listado.outerHTML="<h1>No soy más una lista (código HTML outerHTML) reemplaza etiqueta + contenido</h1>";
+
+// con document.write se puede, es una mala practica porque
+// inserta el texto al final del documento, de la forma
+// anterior se inserta texto exactamente en el lugar que 
+// el proyecto lo requiera
+
+let estacioness = document.querySelector("#estaciones");
+console.log('estacioness es: ', estacioness);
+
+console.log('lista elementos hijos de las estaciones: ', estacioness.children);
+console.log('lista elementos hijo 2 de estaciones: ', estacioness.children[2]);
+console.log('lista elementos padre de las estaciones: ', estacioness.parentElement);
+
+/// hasta acá manejo de etiquetas HTML desde JS 
+
+////////////////
+
+
--- index.js
+++ index.js
@@ -1,84 +1,31 @@
+// desde aca manejo de selectores de css desde JS
 
-//53:00 break
-console.log('muestra los objetos (sin ;)');
-console.log(document)
-console.log(document.doctype)
-console.log(document.documentElement)
-console.log(document.head)
-console.log(document.body)
+document.querySelector(".item").style.color = "blue";
+document.querySelector(".item").style.fontSize = "2rem";
+const itemCSS = document.querySelector(".item");
+console.log(itemCSS.style);
+// muestra los estilos
 
-console.log('muestra el código (con ;)');
+console.log(itemCSS.getAttribute("style"));
+// metodo que permite acceder al contenido del atributo
+// style de una etiqueta de html. Devuelve un array si 
+// hay varias propiedades
 
-console.log(document)
-console.log(document.doctype)
-console.log(document.documentElement)
-console.log(document.head)
-console.log(document.body)
+const enlace = document.querySelector("#estaciones");
+console.log(getComputedStyle(enlace).getPropertyValue("color"));
+// obtengo los estilos de estaciones . muestra lo que hay dentro de la propiedad
 
-console.log('seleccionar un elemento por nombre: "li"');
-console.log(document.getElementsByTagName('li'));
-console.log('seleccionar un elemento por class: "item"');
-console.log(document.getElementsByClassName('item'));
-console.log('seleccionar un elemento por nombre: "name"');
-console.log(document.getElementsByName('name'));
-console.log('seleccionar un elemento por nombre: "phome"');
-console.log(document.getElementsByName('phome'));
-console.log('seleccionar un elemento por nombre: "email"');
-console.log(document.getElementsByName('email'));
-console.log('seleccionar un elemento por id (es único): "titulo"');
-console.log(document.getElementById('titulo'));
+const enlace2 = document.querySelector("#estaciones");
+enlace2.style.setProperty("color", "aquamarine");
+enlace2.style.setProperty("background-color", "red");
+// enlace02.style.setProperty("background-color", "red");
+// enlace02.style.setPproperty("text-decoration", "none");
+console.log(enlace2);
 
-console.log('Seleccionar como css con querySelector');
-console.log('Seleccionar una etiqueta (li: la primera que encuentra)');
-const lista = document.querySelector('li');
-console.log(lista);
-
-console.log('Seleccionar una clase (item: tambien el primero)');
-const item = document.querySelector('.item');
-console.log(item);
-
-console.log('Seleccionar una id (titulo: tambien el primero)');
-const titulo = document.querySelector('#titulo');
-console.log(titulo);
-
-console.log('Seleccionar una clase con todos sus componentes (item)');
-const todosLosItems = document.querySelectorAll('.item');
-console.log(todosLosItems);
-
-console.log('Seleccionar una etiqueta li con todos sus componentes');
-const todosLosLi = document.querySelectorAll('li');
-console.log(todosLosLi);
-
-console.log('Otra forma de mostrar lo anterior (li)');
-const todosLosLis = document.querySelectorAll('li');
-todosLosLis.forEach(li => console.log(li));
-/* equivale a:
-arr.forEach(function callback(currentValue, index, array) {
-    ... tu iterador
-}[, thisArg]);*/
-
-const listaEP = document.querySelector("ul").getAttribute("name");
-// todos los atributos name que se encuentren en la etiqueta ul.
-console.log(listaEP);
-
-// get lee
-// set escribe
-
-const listado = document.querySelector("ol");
-console.log(listado); // muestra todo la etiqueta ol y li
-// setTimeout(1000);
-listado.setAttribute("name", "listaSeteada");
-// muestra el valor del atributo name   ->   listaSeteada
-console.log(listado.getAttribute("name"));
-
-// de esta forma reescribo lo que tiene listado
-listado.textContent="reescribo el contenido de ol (chau li) con formato de texto";
-listado.innerHTML="<h1>Soy una lista (código HTML innerHTML)</h1>";
-listado.outerHTML="<h1>No soy más una lista (código HTML outerHTML)</h1>";
-
-
-
-
+const enlaceS = document.querySelector("#titulo");
+enlaceS.style.backgroundColor = "pink";
+enlaceS.style.color="black";
+enlaceS.style.padding="20px";
 
 
 
